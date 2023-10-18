7c151df5dce81a0d1f0c9f4d150c6ba32d9f2f7e
Owsiej <owsiejaszek@gmail.com>
2023-10-18 18:33:21 +0200

    fixed string-action homework

--- JavaScript_ps/14.10/homework/string-actions.js
+++ JavaScript_ps/14.10/homework/string-actions.js
@@ -1,49 +1,45 @@
-// const text = "This is an example";
-const text ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const text = "This is an example";
+// const text ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-let wordCounter = 0;
+let workingText = text.replaceAll(",", "").replaceAll(".", "");
+let wordCounter = workingText ? 1 : 0;
 let letterCounter = 0;
 
-
-for (let i =0;i<=text.length-1; i++) {
-    if (text[i]==" ") {
-        wordCounter+=1;
-    } else if (text[i].toLowerCase()==="a") {
-        letterCounter+=1;
-
-    }
+for (let i = 0; i <= workingText.length - 1; i++) {
+  if (workingText[i] == " ") {
+    wordCounter++;
+  } else if (workingText[i].toLowerCase() === "a") {
+    letterCounter++;
+  }
 }
 
-console.log("Number of words: "+wordCounter);
-console.log("Number of letter a: "+letterCounter);
-
+console.log("Number of words: " + wordCounter);
+console.log("Number of letter a: " + letterCounter);
 
 let longestWords = "";
 let maxWordLength = 0;
 let spaceIndex = 0;
-let currentWord ="";
+let currentWord = "";
 let longestWordsCount = 0;
-let workingText = text.replaceAll(",", "").replaceAll(".", "")
 
 do {
-    spaceIndex=workingText.indexOf(" ");
-    if (spaceIndex!=-1) {
-        currentWord = workingText.slice(0, spaceIndex);
-        workingText=workingText.slice(spaceIndex+1);
-    } else {
-        currentWord = workingText;
-    }
+  spaceIndex = workingText.indexOf(" ");
+  if (spaceIndex != -1) {
+    currentWord = workingText.slice(0, spaceIndex);
+    workingText = workingText.slice(spaceIndex + 1);
+  } else {
+    currentWord = workingText;
+  }
 
-    if (currentWord.length>maxWordLength) {
-        maxWordLength=currentWord.length;
-        longestWords=currentWord;
-        longestWordsCount=1;
-    } else if (currentWord.length==maxWordLength) {
-        longestWords+=" "+currentWord;
-        longestWordsCount++;
-    }
-} while (spaceIndex!=-1)
+  if (currentWord.length > maxWordLength) {
+    maxWordLength = currentWord.length;
+    longestWords = currentWord;
+    longestWordsCount = 1;
+  } else if (currentWord.length == maxWordLength) {
+    longestWords += " " + currentWord;
+    longestWordsCount++;
+  }
+} while (spaceIndex != -1);
 
 console.log(`Longest words: ${longestWords}`);
 console.log(`Number of longest(${maxWordLength}) words: ${longestWordsCount}`);
-
