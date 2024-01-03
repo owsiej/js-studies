682b6fbc6fe38f6abe4e541cab995a6f7402ef17
Owsiej <owsiejaszek@gmail.com>
2024-01-03 15:06:14 +0100

    added react 6&7 exercise

--- React/2023-12-17/react23-24/src/components/UserForm/UserForm.jsx
+++ React/2023-12-17/react23-24/src/components/UserForm/UserForm.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 
 import "./UserForm.css";
 
+const foodPreferences = ["tofu", "beans", "chicken", "beef", "fish"];
+
 const UserForm = () => {
   const [userName, setUserName] = useState("");
   const [userPassword, setUserPassword] = useState("");
-  const [showPassword, setShowPassowrd] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [age, setAge] = useState();
+  const [isVegan, setIsVegan] = useState(false);
+  const [foodPref, setFoodPref] = useState(
+    new Array(foodPreferences.length).fill(false)
+  );
   const handleChangeUserName = (e) => {
     setUserName(e.target.value);
   };
@@ -21,6 +27,15 @@ const UserForm = () => {
     }
   };
 
+  const handleFoodCheckbox = (pos) => {
+    const updateFoodPref = foodPref.map((value, index) =>
+      index === pos ? !value : value
+    );
+
+    setFoodPref(updateFoodPref);
+    console.log(foodPref);
+  };
+
   return (
     <form className="UserForm">
       <input
@@ -33,14 +48,39 @@ const UserForm = () => {
         value={userPassword}
         onChange={handleChangeUserPassword}
       />
-      <button type="button" onClick={() => setShowPassowrd(!showPassword)}>
+      <button type="button" onClick={() => setShowPassword(!showPassword)}>
         {showPassword ? "Hide password" : "Show password"}
       </button>
       <input
         type="number"
         value={age}
+        placeholder="age"
         onChange={(e) => handleSetAge(e.target.value)}
       />
+      <label>is Vegan?</label>
+      <select
+        name="vegan"
+        value={isVegan}
+        onChange={(e) => setIsVegan(e.target.value)}
+      >
+        <option value="yes">Yes</option>
+        <option value="no">No</option>
+      </select>
+      <label>food Preferences</label>
+      <div>
+        {foodPreferences.map((value, index) => (
+          <label key={index}>
+            <input
+              type="checkbox"
+              name={value}
+              value={value}
+              checked={foodPref[index]}
+              onChange={() => handleFoodCheckbox(index)}
+            />
+            {value}
+          </label>
+        ))}
+      </div>
     </form>
   );
 };
